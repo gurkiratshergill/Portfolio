e686cbd319add3b1f75462ed915b98faa044e1a0
gurkiratshergill <gurkriatshergill@gmail.com>
2025-03-16 19:06:51 +0530

    Revampled the whole portfolio.

--- jobs/views.py
+++ jobs/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Job
-def home(request):
+
+# old view for home page
+# def home(request):
+#     jobs = Job.objects
+#     return render(request, 'jobs/home.html',{'jobs':jobs})
+
+def index(request):
     jobs = Job.objects
-    return render(request, 'jobs/home.html',{'jobs':jobs})
+    return render(request, 'jobs/index.html',{'jobs':jobs})
 
 def detail(request, job_id):
    job_detail = get_object_or_404(Job, pk=job_id)

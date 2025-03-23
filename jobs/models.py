5e7f329d123917fec60aef4f05d9cd42c751d99e
gurkiratshergill <gurkriatshergill@gmail.com>
2025-03-23 21:51:08 +0530

    Changed databas from Postgres to Sqlite. Update the database path in the settings.py file.
    Updated the Project details page.
    
    Made some other minor changes to ui.

--- jobs/models.py
+++ jobs/models.py
@@ -3,7 +3,10 @@ from django.db import models
 # Create your models here.
 class Job(models.Model):
     image = models.ImageField(upload_to="images/")
-    summary = models.CharField(max_length=200)
+    image2 = models.ImageField(upload_to="images/",default='images/None/no-img.jpg')
+    image3 = models.ImageField(upload_to="images/",default='images/None/no-img.jpg')
+    title = models.CharField(max_length=100, default='Title')
+    summary = models.CharField(max_length=10000)
 
     def __str__(self):
         return self.summary
--- portfolio/settings.py
+++ portfolio/settings.py
@@ -76,13 +76,17 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql',
+    #     'NAME':'portfoliodb',
+    #     'USER': 'postgres',
+    #     'PASSWORD': '1234',
+    #     'HOST': 'localhost',
+    #     'PORT': '5432',
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME':'portfoliodb',
-        'USER': 'postgres',
-        'PASSWORD': '1234',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
